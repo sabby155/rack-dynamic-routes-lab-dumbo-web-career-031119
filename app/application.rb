cd6c6f37cee40cee558aeb5c7408263b3aeddace
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:56:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     item_name = req.path.split("/items/").last 
     item = @@item.find {|i| i.name == item_name}
     resp.write item.price
-    resp.status =   
+   
     end   
     
     resp.finish
