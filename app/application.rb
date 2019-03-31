3d661dd3d490926ec075aca552e0fbea977c5d9b
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 19:27:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,51 +1,28 @@
-class Application 
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Response.new(env)
+ 
+
+
+class Application
     
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
 
-      if req.path.match(/items/)
-        item_name = req.path.split("/items/").last 
-        
-        @@items.find do |item|
-            if item.name == item_name
-              resp.write item.price
-            elsif item != @@items.last 
-              resp.write "Error: Item not found"
-              resp.status = 400
-            end  
-          end
-        else 
-          resp.write "Error: Route not found."
-          resp.status = 404 
-      end   
-    resp.finish
-  end   
-end   
+        if req.path.match(/items/)
+            the_item = req.path.split("/items/").last
 
-
-# class Application
-    
-#     def call(env)
-#         resp = Rack::Response.new
-#         req = Rack::Request.new(env)
-
-#         if req.path.match(/items/)
-#             the_item = req.path.split("/items/").last
-
-#             @@items.find{|item| 
-#                 if item.name == the_item
-#                     resp.write item.price
-#                 elsif item == @@items.last
-#                     resp.write "Error: Item not found"
-#                     resp.status = 400
-#                 end
-#             }
-#         else
-#             resp.write "Error: Route not found."
-#             resp.status = 404
-#         end
-#         resp.finish
-#     end
-# end
+            @@items.find{|item| 
+                if item.name == the_item
+                    resp.write item.price
+                elsif item == @@items.last
+                    resp.write "Error: Item not found"
+                    resp.status = 400
+                end
+            }
+        else
+            resp.write "Error: Route not found."
+            resp.status = 404
+        end
+        resp.finish
+    end
+end
   
