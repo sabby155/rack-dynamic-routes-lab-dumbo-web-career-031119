cbe301f3766b7a4c41b23baddf99faad4a3e4b8d
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:51:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
     
     item_name = req.path.split("/items/").last 
-    item = @@items.find {|i| i.}
+    item = @@items.find {|i| i.name == item.name}
       
     end   
     
