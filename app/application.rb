64925f37769395b96543c0a6d42affd2f93778a2
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:53:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Response.new(env)
     
-    @@items = []
+    # @@item = []
     
     if req.path.match(/items/)
     
     item_name = req.path.split("/items/").last 
-    item = @@items.find {|i| i.name == item_name}
+    item = @@item.find {|i| i.name == item_name}
     resp.write item.price
       
     end   
