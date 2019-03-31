ed431d3e2701c6aa77dd7b0a436e329dcf6bd5b6
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 18:58:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,13 +5,14 @@ class Application
     
     # @@item = []
     
-    if req.path == "/items/"
-    
-      item_name = req.path.split("/items/").last 
-      item = @@item.find {|i| i.name == item_name}
-      resp.write item.price
-    else 
-      resp.status = 404
+      if req.path == "/items/"  
+        item_name = req.path.split("/items/").last 
+     if @@item.include?(item_name)  
+       item = @@item.find {|i| i.name == item_name}
+        resp.write item.price
+     else 
+       resp.status = 404
+     end   
    
     end   
     
